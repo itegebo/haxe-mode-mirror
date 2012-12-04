327a354d9e4b3d7c5e09af72c0c0cd1401986bd7
olegsivokon@gmail.com <olegsivokon@gmail.com@67cdf64b-8657-2e48-644e-950c9fbc8547>
2012-12-04 14:06:23 +0000

    Forgot thingatpt
    git-svn-id: http://haxe-mode.googlecode.com/svn/trunk@42 67cdf64b-8657-2e48-644e-950c9fbc8547

--- haxe-mode-pkg.el
+++ haxe-mode-pkg.el
@@ -3,4 +3,4 @@
                 '((flymake "0.3")
                   (cl "2.2") (eieio "1.3")
                   (ede "1.0") (semantic "2.0")
-                  (thingatpt "") (yasnippet "20121127")))
+                  (yasnippet "20121127")))
